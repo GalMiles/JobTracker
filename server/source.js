1657fb1e6436e98752a072bf5bc3e2780060dde0
Gal Miles <58370322+GalMiles@users.noreply.github.com>
2023-09-25 21:30:02 +0300

    adding server file

---
+++ server/source.js
@@ -0,0 +1,87 @@
+const { get } = require('http');
+const Imap = require('imap');
+const { simpleParser } = require('mailparser');
+
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
+
+const imap = new Imap({
+  user: 'gal.miles.career@gmail.com',
+  password: 'wczw urpm ehcd ploo',
+  host: 'imap.gmail.com',
+  port: 993,
+  tls: true,
+  tlsOptions: { rejectUnauthorized: false },
+  connTimeout: 10000, // Set a longer connection timeout (e.g., 10 seconds)
+  authTimeout: 5000  // Set a longer authentication timeout (e.g., 5 seconds)
+});
+
+// // Function to handle errors
+// function handleError(err) {
+//   console.error('Error:', err);
+// }
+
+
+
+// // Handle the 'ready' event, which fires when the connection is established
+
+
+// // Handle the 'error' event
+// imap.once('err', handleError);
+
+// // Handle the 'close' event, which fires when the connection is closed
+// imap.once('close', () => {
+//   console.log('Connection to Gmail IMAP server closed.');
+// });
+
+
+getEmails = () => {
+  try {
+    imap.once('ready', () => {
+      console.log('Connected to Gmail IMAP server.');
+
+      // List the available mailboxes
+      imap.openBox('INBOX', false, () => {
+        imap.search(['UNSEEN'], (err, results) => {
+          // imap.search(['UNSEEN', ['SINCE', new Date()]], (err, results) => {
+          const f = imap.fetch(results, { bodies: '' });
+          f.on('message', msg => {
+            msg.on('body', stream => {
+              simpleParser(stream, async (err, parsed) => {
+                console.log('==================================');
+                console.log('From:' + parsed.from.text + '->' + parsed.text);
+              })
+            })
+          })
+          // Handle the 'end' event, which fires when the connection ends
+          imap.once('end', () => {
+            console.log('Done fetching mails.');
+            imap.end();
+          });
+        });
+
+      });
+    });
+
+
+    imap.once('error', err => {
+      console.log(err);
+    });
+    
+    imap.once('end', () => {
+      console.log('Disconnected from Gmail IMAP server.');
+    });
+
+    // Connect to the IMAP server
+    imap.connect();
+
+    // // // Disconnect when done
+    // imap.end();
+  }
+  catch (e) {
+    console.log('Error has occuerd:' + e);
+  }
+}
+
+getEmails();
+
+
